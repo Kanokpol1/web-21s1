2353cd18377df60d4b9d9eb264454df8c3ac27f6
Joker <86314854+Kanokpol1@users.noreply.github.com>
2021-07-09 11:16:36 +0700

    [U3] Navigation breadcrumb include

--- u3-views/features/courseController.js
+++ u3-views/features/courseController.js
@@ -7,7 +7,12 @@ const courseDetails = (req, res) => {
   // left `coure` = neme in handlebars
   // right `coure` = JS variable
 
-  res.render('course-details', { course: course })
+  res.render('course-details', {
+    course: course,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode }
+    ]
+  })
 }
 
 const courseUnits = (req, res) => {
@@ -17,14 +22,29 @@ const courseUnits = (req, res) => {
   // left `units` = neme in handlebars
   // right `coure.units` = JS expression
 
-  res.render('course-units', { units: course.units })
+  res.render('course-units', {
+    course: course,
+    units: course.units,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode },
+      { href: `/courses/${courseCode}/units`, text: 'Units' }
+    ]
+  })
 }
 
 const courseStudent = (req, res) => {
   const courseCode = req.params.code
   const course = getCourse(courseCode)
-  res.render('course-students', { course: course })
+
+  res.render('course-students', {
+    course: course,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode },
+      { href: `/courses/${courseCode}/students`, text: 'Students' }
+    ]
+  })
 }
+
 module.exports = {
   courseDetails,
   courseUnits,
