6bc91cc5428639a733bbfc9378ef60d32114de07
Joker <86314854+Kanokpol1@users.noreply.github.com>
2021-07-24 14:16:27 +0700

    [L5] Finist

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { json, urlencoded } = require('body-parser')
 
 const { readBooks, readBook, createBook, replaceBook, updateBook, deleteBook } = require('./features/book-controller')
+const { covies, covie, createCovie, updateCovie, deleteCovie } = require('./features/covid-controller')
 
 const app = express()
 
@@ -17,5 +18,12 @@ app.post('/books', createBook)
 app.put('/books/:isbn13', replaceBook)
 app.patch('/books/:isbn13', updateBook)
 app.delete('/books/:isbn13', deleteBook)
+
+// Routes Lab
+app.get('/covid', covies)
+app.get('/covid/:stateId', covie)
+app.post('/covid', createCovie)
+app.patch('/covid/:stateId', updateCovie)
+app.delete('/covid/:stateId', deleteCovie)
 
 module.exports = { app }
