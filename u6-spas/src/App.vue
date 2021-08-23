3cea635e04cbbee1a50f6f650395d28d7834873d
Joker <86314854+Kanokpol1@users.noreply.github.com>
2021-08-23 08:32:24 +0700

    [U6] Routes, MVVM, Binding, V-for, @click

---
+++ u6-spas/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'Colors' }">Colors</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
