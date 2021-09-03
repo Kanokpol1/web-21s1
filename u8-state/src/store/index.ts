38bd2e3fd64de9b9837647604d8e6930b3e97997
Joker <86314854+Kanokpol1@users.noreply.github.com>
2021-09-03 21:51:22 +0700

    [U8L] Finish

--- u8-state/src/store/index.ts
+++ u8-state/src/store/index.ts
@@ -1,18 +1,66 @@
+import { db } from '@/_services/firebase-initialized'
+import { keyBy, sumBy } from 'lodash'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { vuexfireMutations } from 'vuexfire'
+import { firestoreAction, vuexfireMutations } from 'vuexfire'
+import { Video, Book } from './models'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export interface Store {
+  books: Book[]
+  cartIsbn13s: string[],
+  videos: Video[]
+  watchLater: string[]
+}
+
+const store = new Vuex.Store<Store>({
   state: {
-    getters: {},
+    books: [],
+    cartIsbn13s: [],
+    videos: [],
+    watchLater: []
   },
+  getters: {
+    videosById: state => keyBy(state.videos, 'id'),
+    watchLaterCount: state => state.watchLater.length,
+    watchLaterFull: (state, getters) => state.watchLater.map(
+      id => getters.videosById[id]
+    ),
+
+    booksByIsbn13: state => keyBy(state.books, 'isbn13'),
+    cart: (state, getters) => state.cartIsbn13s.map(isbn13 =>
+      getters.booksByIsbn13[isbn13]),
+    cartCount: state => state.cartIsbn13s.length,
+    cartTotal: (_state, getters) => sumBy(getters.cart, 'price')
+
+  },
+
   mutations: {
-     ...vuexfireMutations
+    ...vuexfireMutations,
+    'watchLater/push': (state, id: string) => {
+      if (state.watchLater.includes(id)) return
+      state.watchLater.push(id)
+    },
+    'watchLater/splice': (state, id: string) => {
+      const index = state.watchLater.indexOf(id)
+      if (index === -1) return
+      state.watchLater.splice(index, 1)
+    },
+    'cartIsbn13s/push': (state, isbn13: string) => {
+      if (state.cartIsbn13s.includes(isbn13)) return
+      state.cartIsbn13s.push(isbn13)
+    }
   },
   actions: {
+    init: firestoreAction(({ bindFirestoreRef }) => Promise.all([
+      bindFirestoreRef('videos', db.collection('videos')),
+      bindFirestoreRef('books', db.collection('books'))
+    ])),
+    addToWatchLater: ({ commit }, id: string) => commit('watchLater/push', id),
+    addToCart: ({ commit }, isbn13: string) => commit('cartIsbn13s/push', isbn13)
   },
-  modules: {
-  }
+  modules: { }
 })
+store.dispatch('init')
+export default store
