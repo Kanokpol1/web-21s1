abf821e103572039f33c71ac72ca2f8943858a1d
Joker <86314854+Kanokpol1@users.noreply.github.com>
2021-09-11 17:39:49 +0700

    [U9] Vue Homepage

--- u9-css/src/main.ts
+++ u9-css/src/main.ts
@@ -4,13 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@mdi/font/css/materialdesignicons.min.css'
-import './main.css'
+import './main.scss'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+new Vue({ router, store, render: h => h(App) }).$mount('#app')
